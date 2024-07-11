a297b719a319e52f34a15fdb3c287b1daf838212
Muhammad Qasim <malikqasim20051@gmail.com>
2024-07-11 11:01:37 +0500

    Dynamic All-Items-Owl-Carousel completed

--- static/Javascript/app.js
+++ static/Javascript/app.js
@@ -18,6 +18,28 @@ $('.owl-carousel-1').owlCarousel({
 
 
 
+$('.owl-carousel-2').owlCarousel({
+    loop:false,
+    nav:true,
+    margin:0,
+    autoplay: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+
+
+
+
 
 
 
